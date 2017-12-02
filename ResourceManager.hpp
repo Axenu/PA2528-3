9e30d50c7ee36ed157d1b71157c981c2182d5038
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-12-02 14:04:10 +0100

    AsyncHandle returns SharedPtr

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -9,27 +9,29 @@
 // Asynchronous operations are handled by the ResourceManager. Not the PackageReader.
 class ResourceManager {
     public:
+        static void initialize();
+
         // Synchronous load methods.
         static SharedPtr<Texture> loadTexture(gui_t gui);
         static SharedPtr<Mesh> loadMesh(gui_t gui);
 
         // Asynchronous load methods.
+        template<typename T>
         struct AsyncHandle {
-            // Return true if ready, else false. Will wait (block) for specified duration.
-            bool wait(size_t milliseconds);
+            // Return pointer of resource if ready, else nullptr. Will wait (block) for specified duration.
+            SharedPtr<T> wait(size_t milliseconds);
         };
         // Something like this... Poll the handle to check availability.
-        static AsyncHandle aloadTexture(gui_t gui);
-        static AsyncHandle aloadMesh(gui_t gui);
+        static AsyncHandle<Texture> aloadTexture(gui_t gui);
+        static AsyncHandle<Mesh> aloadMesh(gui_t gui);
 
         template<typename T>
         using Callback = void (*)(SharedPtr<T>);
-        using TextureCallback = Callback<Texture>;
-        using MeshCallback = Callback<Mesh>;
-
         // Something like this... The callback is invoked when operation completes.
-        static aloadTexture(gui_t gui, TextureCallback callback);
-        static aloadMesh(gui_t gui, MeshCallback callback);
+        // The SharedPtr parameter of the callback contains the loaded resource.
+        static aloadTexture(gui_t gui, Callback<Texture> callback);
+        static aloadMesh(gui_t gui, Callback<Mesh> callback);
+
 
 
 };
