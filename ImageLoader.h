9c49ce31a40714b67db442e16924ad03c4c397af
Johan91 <j.lindgren1991@gmail.com>
2017-12-05 20:32:54 +0100

    moved all imageloader related code into the header file

--- ImageLoader.h
+++ ImageLoader.h
@@ -10,8 +10,30 @@
 class ImageLoader
 {
 public:
-	ImageLoader();
-	~ImageLoader();
+	//unsigned char*  ImageLoader::loadFromFile(const char* file)
+	static unsigned char* loadFromFile(const char* file, int* width, int* height)
+	{
+		unsigned char* image = stbi_load(file, width, height, 0, 0);
+
+		// process data
+
+		return image;
+	}
+
+	static unsigned char* loadFromMemory(int length, int* width, int* height)
+	{
+		const stbi_uc* buffer;
+
+		unsigned char* image = stbi_load_from_memory(buffer, length, width, height, 0, 0);
+
+		return image;
+	}
+
+	static void freeImage(unsigned char* image)
+	{
+		if (image)
+			stbi_image_free(image);
+	}
 };
 
 #endif
