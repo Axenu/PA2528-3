7db2245c5fc11026e1eae76c5a111443cddc5d8d
Timmie Pettersson <timmie.pettersson@gmail.com>
2017-12-06 13:06:44 +0100

    Added initial PackageReader implementation
    Does not support reading from memory. GUI's don't work.

--- PackageReader.cpp
+++ PackageReader.cpp
@@ -3,22 +3,165 @@
 #include "Texture.hpp"
 #include "Mesh.hpp"
 
+
+std::string PackageReader::packagePath;
+Array<PackageReader::MetaData> PackageReader::metaData;
+size_t PackageReader::numResourcesInPackage = 0;
+void* PackageReader::rawFile = nullptr;
+std::ifstream PackageReader::file;
+size_t PackageReader::baseOffset = 0;
+
+
 // Return true if package is found and valid. Else false.
 bool PackageReader::setPackage(const char* path) {
-    return false;
+	// Clear current package, if any.
+	clearPackage();
+
+	packagePath = path;
+
+	// Open file
+	openFile();
+
+	file >> numResourcesInPackage;
+	file.get(); // Removes newline
+	
+	metaData.data = new PackageReader::MetaData[numResourcesInPackage];
+	metaData.size = numResourcesInPackage;
+
+	const int maxStringLength = 300;
+	char* temp = new char[maxStringLength];
+	for (size_t i = 0; i < numResourcesInPackage; ++i) {
+		// Read filename
+		file.getline(temp, maxStringLength, ':');
+
+		// Find extension
+		const char* dot = strrchr(temp, '.');
+		
+		// If there is no extension, the package file is invalid.
+		if (!dot) {
+			closeFile();
+			delete[] temp;
+			return false;
+		}
+
+		// Set appropriate resource type.
+		if (strcmp(dot, ".obj") == 0) {
+			metaData.data[i].type = PackageReader::MetaData::Type::MESH;
+		}
+		else if (strcmp(dot, ".png") == 0) {
+			metaData.data[i].type = PackageReader::MetaData::Type::TEXTURE;
+		}
+		// TODO: add more file types?
+
+		// Read gui
+		file.getline(temp, maxStringLength, ':');
+
+		// Convert to bit representation and store in metadata array
+		// TODO: fix me pls
+		metaData.data[i].gui = strtoll(temp, NULL, 16);
+
+		// Read size
+		file.getline(temp, maxStringLength, ':');
+		metaData.data[i].size = strtol(temp, NULL, 10);
+
+		// Read offset
+		file.getline(temp, maxStringLength);
+		metaData.data[i].offset = strtol(temp, NULL, 10);
+	}
+	// Set base offset to the next position (the start of the first file)
+	baseOffset = static_cast<size_t>(file.tellg());
+
+	closeFile();
+	delete[] temp;
+	return true;
+}
+
+// Clears the current package. Used when exiting application or switching package.
+void PackageReader::clearPackage() {
+	delete rawFile;
+	rawFile = nullptr;
+
+	delete[] metaData.data;
+	metaData.data = nullptr;
+	metaData.size = 0;
 }
 
 // Return pointer to texture resource with specified GUI. Return nullptr if not found.
 Texture* PackageReader::loadTexture(gui_t gui) {
-    return new Texture();
+	void* mem = loadFile(gui);
+    
+	if (!mem)
+		return nullptr;
+
+	// Read memory into new texture
+
+	free(mem);
+
+	return nullptr;	// return texture
 }
 
 // Return pointer to mesh resource with specified GUI. Return nullptr if not found.
 Mesh* PackageReader::loadMesh(gui_t gui) {
-    return new Mesh();
+	void* mem = loadFile(gui);
+
+	if (!mem)
+		return nullptr;
+
+	// Read memory into new mesh
+
+	free(mem);
+
+	return nullptr;	// return mesh
 }
 
-// Return array of all GUIs in the package.
+// Return array of all GUIs in the package. Caller is responsible for the memory of returned array.
 Array<PackageReader::MetaData> PackageReader::getMetaData() {
-    return Array<MetaData>();
+	Array<MetaData> newArr;
+	newArr.size = metaData.size;
+	newArr.data = new PackageReader::MetaData[metaData.size];
+
+	memcpy(newArr.data, metaData.data, sizeof(PackageReader::MetaData) * metaData.size);
+
+    return newArr;
+}
+
+bool PackageReader::openFile()
+{
+	// Remove buffer
+	file.rdbuf()->pubsetbuf(0, 0);
+
+	file.open(packagePath);
+
+	// Return the file's open status
+	return file.is_open();
+}
+
+void PackageReader::closeFile()
+{
+	if (file.is_open())
+		file.close();
+}
+
+// Searches for and loads a file from the package into memory. Caller is responsible for the returned memory.
+void* PackageReader::loadFile(gui_t gui)
+{
+	for (size_t i = 0; i < numResourcesInPackage; ++i) {
+		if (metaData.data[i].gui == gui) {
+			openFile();
+
+			file.seekg(metaData.data[i].offset + baseOffset);
+
+			// Allocate memory for the file
+			char* mem = static_cast<char*>(malloc(metaData.data[i].size));
+
+			// Read the file into memory
+			file.read(mem, metaData.data[i].size);
+
+			closeFile();
+
+			return mem;
+		}
+	}
+
+	return nullptr;
 }
--- PackageReader.hpp
+++ PackageReader.hpp
@@ -2,6 +2,7 @@
 #define __PACKAGE_READER__
 
 #include <string>
+#include <fstream>
 #include "Array.hpp"
 #include "gui_t.hpp"
 
@@ -24,11 +25,16 @@ class PackageReader {
 
             Type type = Type::INVALID;
             gui_t gui = 0;
+			size_t offset;	// Number of bytes from the first file in the package to the start of this file.
+			size_t size;	// Size of the file.
         };
 
     public:
         // Return true if package is found and valid. Else false.
         static bool setPackage(const char* path);
+
+		// Clears the current package. Used when exiting application or switching package.
+		static void clearPackage();
 
         // Return pointer to texture resource with specified GUI. Return nullptr if not found.
         static Texture* loadTexture(gui_t gui);
@@ -36,9 +42,27 @@ class PackageReader {
         // Return pointer to mesh resource with specified GUI. Return nullptr if not found.
         static Mesh* loadMesh(gui_t gui);
 
-        // Return metadata of every resource in the package.
+        // Return metadata of every resource in the package. Caller is responsible for the memory of returned array.
         static Array<MetaData> getMetaData();
 
+	private:
+		// Opens and closes the package file.
+		static bool openFile();
+		static void closeFile();
+
+		// Searches for and loads a file from the package into memory. Caller is responsible for the returned memory.
+		static void* loadFile(gui_t gui);
+
+		static std::string packagePath;
+		static Array<MetaData> metaData;
+		static size_t numResourcesInPackage;
+		static std::ifstream file;
+
+		// The location of the beginning of the first file.
+		static size_t baseOffset;
+
+		// Contains a resource file copied from disk to be converted into a mesh/image.
+		static void* rawFile;
 };
 
 #endif // __PACKAGE_READER__
